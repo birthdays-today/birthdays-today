b4b72cc42b4e9ab24c87ea74992e844703253659
William Weiskopf <william@weiskopf.me>
2020-05-12 21:16:22 -0400

    Selects a random birthday from the present day.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,17 @@
 const person = document.getElementById('person');
 
+const today = new Date();
+const month = today.toLocaleString('default', { month: 'long' });
+const day = today.getDate();
+
 Papa.parse(
-  'https://storage.googleapis.com/birthdays_today/csv/birthday.csv', {
+  `https://storage.googleapis.com/birthdays_today/csv/${month}_${day}.csv`, {
     download: true,
     header: true,
     complete: function(result) {
-      console.log(result.data);
-      console.log(result.data.length);
-      console.log(Math.floor(Math.random() * result.data.length));
       const individual = result.data[Math.floor(Math.random() * result.data.length)];
-      console.log(individual)
-
       person.textContent = individual.name;
       person.href = individual.URL;
     }
-  });
-console.log(data)
-//data = Papa.parse('csv_birthday.csv', {download: true, header: true});
-
-individual = data[Math.floor(Math.random() * data.length)];
-
-person.textContent = individual.name;
-person.href = individual.URL;
-//} else {
-//  const errorMessage = document.createElement('marquee');
-//  errorMessage.textContent = `Gah, it's not working!`;
-//  app.appendChild(errorMessage);
-//}
+  }
+);
